3760b8acaaf1425b654f79ff72507a1e0b75caf2
Lais <91293868+laisfbs@users.noreply.github.com>
2022-11-17 14:06:59 -0300

    Add files via upload

---
+++ DiminuiLetras/src/DiminuiLetras.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 40415873
+ */
+public class DimunuiLetras {
+    public static void lowCase() throws NullPointerException
+{
+    String word = null;
+    String newWord = null;
+
+    newWord = word.toLowerCase();
+
+    System.out.println("A palavra informada é " + word);
+
+    }
+}
